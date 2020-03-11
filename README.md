9988d595aa987a7fc31555a312d6e07d4935591f
Priyansh Saxena <askpriyansh@gmail.com>
2020-03-11 18:34:06 +0530

    issue_11: Setup code-coverage reporting (#12)
    Closes #11

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # native-speaker
+
+![](https://github.com/priyanshsaxena/native-speaker/workflows/Native-Speaker%20CI/badge.svg)
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
     setupFiles: ['<rootDir>/jest.setup.js'],
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
     collectCoverage: true,
+    coverageReporters: [
+        "json-summary",
+        "text",
+        "lcov"
+    ],
     collectCoverageFrom: [
         '**/*.{js,jsx,ts,tsx}',
         '!**/*.d.ts',
